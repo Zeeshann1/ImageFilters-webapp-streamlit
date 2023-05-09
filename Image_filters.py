682d338773bf9204dcbdee626d59f11c6aa8f99c
Zeeshan Hyder <109053785+Zeeshann1@users.noreply.github.com>
2023-05-09 14:22:33 +0800

    Update Image_filters.py

--- Image_filters.py
+++ Image_filters.py
@@ -15,7 +15,7 @@ from PIL import Image
 
 from io import BytesIO
 import base64
-#from rembg import remove
+from rembg import remove
 
 ################################################### Apply Filters#############################
 from PIL import Image, ImageFilter            ######## IMPORT MODULE
@@ -26,7 +26,7 @@ st.title('Apply Filters to Image')
 
 st.sidebar.write("## Upload and download :gear:")
 
-source = ("CONTOUR FILTER", "SMOOTH MORE FILTER", "SHARPEN FILTER", "EDGE ENHANCE MORE FILTER", " DETAIL FILTER", "EMBOSS FILTER","BLUR FILTER", "ALL")
+source = ("CONTOUR FILTER", "SMOOTH MORE FILTER", "SHARPEN FILTER", "EDGE ENHANCE MORE FILTER", " DETAIL FILTER", "EMBOSS FILTER","BLUR FILTER", "REMOVE BACKGROUND" "ALL")
 source_index = st.sidebar.selectbox("Select Filter", range(
         len(source)), format_func=lambda x: source[x])
 
@@ -52,7 +52,7 @@ if source_index == 0:
         if uploaded_file is not None:
                  st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
                  st.image(uploaded_file)
-                 st.caption("<p style='color:red;font-weight:bold'>CONTOUR FILTER</p>", unsafe_allow_html=True)
+                 st.caption("<p style='color:blue;font-weight:bold'>CONTOUR FILTER</p>", unsafe_allow_html=True)
                  img1 = picture.filter(ImageFilter.CONTOUR)
                  st.image(img1)
                  st.sidebar.markdown("\n")
@@ -63,7 +63,7 @@ if source_index == 1:
         if uploaded_file is not None:
                  st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
                  st.image(uploaded_file)
-                 st.caption("<p style='color:red;font-weight:bold'>SMOOTH MORE FILTER</p>", unsafe_allow_html=True)
+                 st.caption("<p style='color:blue;font-weight:bold'>SMOOTH MORE FILTER</p>", unsafe_allow_html=True)
                  img2 = picture.filter(ImageFilter.SMOOTH_MORE)
                  st.image(img2)
                  st.sidebar.markdown("\n")
@@ -74,7 +74,7 @@ if source_index == 2:
         if uploaded_file is not None:
                  st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
                  st.image(uploaded_file)
-                 st.caption("<p style='color:red;font-weight:bold'> SHARPEN FILTER</p>", unsafe_allow_html=True)
+                 st.caption("<p style='color:blue;font-weight:bold'> SHARPEN FILTER</p>", unsafe_allow_html=True)
                  img3 = picture.filter(ImageFilter.SHARPEN)
                  st.image(img3)
                  st.sidebar.markdown("\n")
@@ -85,7 +85,7 @@ if source_index == 3:
         if uploaded_file is not None:
                  st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
                  st.image(uploaded_file)
-                 st.caption("<p style='color:red;font-weight:bold'>EDGE ENHANCE MORE FILTER</p>", unsafe_allow_html=True)
+                 st.caption("<p style='color:blue;font-weight:bold'>EDGE ENHANCE MORE FILTER</p>", unsafe_allow_html=True)
                  img4 = picture.filter(ImageFilter.EDGE_ENHANCE_MORE)
                  st.image(img4)
                  st.sidebar.markdown("\n")
@@ -96,7 +96,7 @@ if source_index == 4:
         if uploaded_file is not None:
                  st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
                  st.image(uploaded_file)
-                 st.caption("<p style='color:red;font-weight:bold'>DETAIL FILTER</p>", unsafe_allow_html=True)
+                 st.caption("<p style='color:blue;font-weight:bold'>DETAIL FILTER</p>", unsafe_allow_html=True)
                  img5 = picture.filter(ImageFilter.DETAIL)
                  st.image(img5)
                  st.sidebar.markdown("\n")
@@ -107,7 +107,7 @@ if source_index == 5:
         if uploaded_file is not None:
                  st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
                  st.image(uploaded_file)
-                 st.caption("<p style='color:red;font-weight:bold'>EMBOSS FILTER</p>", unsafe_allow_html=True)
+                 st.caption("<p style='color:blue;font-weight:bold'>EMBOSS FILTER</p>", unsafe_allow_html=True)
                  img6 = picture.filter(ImageFilter.EMBOSS)
                  st.image(img6)
                  st.sidebar.markdown("\n")
@@ -119,51 +119,63 @@ if source_index == 6:
         if uploaded_file is not None:
                  st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
                  st.image(uploaded_file)
-                 st.caption("<p style='color:red;font-weight:bold'>BLUR FILTER</p>", unsafe_allow_html=True)
+                 st.caption("<p style='color:blue;font-weight:bold'>BLUR FILTER</p>", unsafe_allow_html=True)
                  img7 = picture.filter(ImageFilter.GaussianBlur(radius=2))
                  st.image(img7)
                  st.sidebar.markdown("\n")
-                 st.sidebar.download_button("Download Result Image", convert_image(img7), "Result.png", "image/png")       
+                 st.sidebar.download_button("Download Result Image", convert_image(img7), "Result.png", "image/png")    
+                        
+################################# Background Remove ##################################################
+
+if source_index == 7: 
+    if uploaded_file3 is not None:
+                 st.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
+                 st.image(uploaded_file)
+                 fixed = remove(uploaded_file)
+                 col1.write("Original Image :wrench:")
+                 st.image(fixed)
+                 st.sidebar.markdown("\n")
+                 st.sidebar.download_button("Download Result Image", convert_image(img7), "Result.png", "image/png")                       
 
 ################################# ALL FILTERS ##################################################
 
-if source_index == 7:
+if source_index == 8:
     if uploaded_file is not None:
         col1.caption("<p style='color:red;font-weight:bold'>ORIGINAL IMAGE</p>", unsafe_allow_html=True)
         col1.image(uploaded_file)
         col1.download_button("Download Result", convert_image(picture), "Result.png", "image/png")
 
-        col1.caption("<p style='color:red;font-weight:bold'>CONTOUR FILTER</p>", unsafe_allow_html=True)
+        col1.caption("<p style='color:blue;font-weight:bold'>CONTOUR FILTER</p>", unsafe_allow_html=True)
         img1 = picture.filter(ImageFilter.CONTOUR)
         col1.image(img1)
         col1.download_button("Download Result", convert_image(img1), "Result.png", "image/png")
 
-        col2.caption("<p style='color:red;font-weight:bold'>SMOOTH MORE FILTER</p>", unsafe_allow_html=True)
+        col2.caption("<p style='color:blue;font-weight:bold'>SMOOTH MORE FILTER</p>", unsafe_allow_html=True)
         img2 = picture.filter(ImageFilter.SMOOTH_MORE)
         col2.image(img2)
         col2.download_button("Download Result", convert_image(img2), "Result.png", "image/png")
 
-        col3.caption("<p style='color:red;font-weight:bold'>SHARPEN FILTER</p>", unsafe_allow_html=True)
+        col3.caption("<p style='color:blue;font-weight:bold'>SHARPEN FILTER</p>", unsafe_allow_html=True)
         img3 = picture.filter(ImageFilter.SHARPEN)
         col3.image(img3)
         col3.download_button("Download Result", convert_image(img3), "Result.png", "image/png")
 
-        col1.caption("<p style='color:red;font-weight:bold'>EDGE ENHANCE FILTER</p>", unsafe_allow_html=True)
+        col3.caption("<p style='color:blue;font-weight:bold'>EDGE ENHANCE FILTER</p>", unsafe_allow_html=True)
         img4 = picture.filter(ImageFilter.EDGE_ENHANCE_MORE)
         col3.image(img4)
         col3.download_button("Download Result", convert_image(img4), "Result.png", "image/png")
 
-        col2.caption("<p style='color:red;font-weight:bold'>DETAIL FILTER</p>", unsafe_allow_html=True)
+        col2.caption("<p style='color:blue;font-weight:bold'>DETAIL FILTER</p>", unsafe_allow_html=True)
         img5 = picture.filter(ImageFilter.DETAIL)
         col2.image(img5)
         col2.download_button("Download Result", convert_image(img5), "Result.png", "image/png")
 
-        col3.caption("<p style='color:red;font-weight:bold'>EMBOSS FILTER</p>", unsafe_allow_html=True)
+        col3.caption("<p style='color:blue;font-weight:bold'>EMBOSS FILTER</p>", unsafe_allow_html=True)
         img6 = picture.filter(ImageFilter.EMBOSS)
         col3.image(img6)
         col3.download_button("Download Result", convert_image(img6), "Result.png", "image/png")
 
-        col2.caption("<p style='color:red;font-weight:bold'>BLUR FILTER</p>", unsafe_allow_html=True)
+        col2.caption("<p style='color:blue;font-weight:bold'>BLUR FILTER</p>", unsafe_allow_html=True)
         img7 = picture.filter(ImageFilter.GaussianBlur(radius=2))
         col2.image(img7)
         col2.download_button("Download Result", convert_image(img7), "Result.png", "image/png")
